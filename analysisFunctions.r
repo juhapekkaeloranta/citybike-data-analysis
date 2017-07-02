f02c584e983e741f31182ffdf81b44a1cf972179
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2017-07-02 16:02:27 +0300

    Change to linegraphs. Include min, max, mean.

--- analysisFunctions.r
+++ analysisFunctions.r
@@ -24,7 +24,6 @@ draw_plot <- function(DF, stationID) {
   stationName <- stations[stations$stationId==stationID,]$name
   barplot(matr, main=stationName, 
           xlab="Time of day", ylab="bikes available",
-          names.arg=humanReadableObservationTimes,
           col='blue')
 }
 
@@ -37,9 +36,86 @@ draw_plot_animate <- function(DF, stationIds) {
 
 ### calculations ###
 
-stationMeans <- function(df) {
-  means <- data.frame(t(colMeans(df, na.rm = FALSE, dims = 1)))
-  colnames(means) <- colnames(df)
-  return(means)
+columnMax <- function(df) {
+  result <- matrix(ncol=NCOL(df), nrow=1)
+  for (j in 1:NCOL(df)) {
+    result[1,j] <- max(df[,j])
+  }
+  return(result)
 }
 
+columnMin <- function(df) {
+  result <- matrix(ncol=NCOL(df), nrow=1)
+  for (j in 1:NCOL(df)) {
+    result[1,j] <- min(df[,j])
+  }
+  return(result)
+}
+
+
+stationMeansHourly <- function(df) {
+  resultDf <- as.data.frame(matrix(ncol=NCOL(df), nrow=0))
+  for (i in 0:23) {
+    hourlyDfSubset <- df[df$hour==i,]
+    indexOfNextRow <- nrow(resultDf) + 1
+    resultDf[indexOfNextRow, ] <- t(colMeans(hourlyDfSubset, na.rm = FALSE, dims = 1))
+    rownames(resultDf)[indexOfNextRow] <- i+2
+  }
+  colnames(resultDf) <- colnames(df)
+  return(resultDf)
+}
+
+stationMaxsHourly <- function(df) {
+  resultDf <- as.data.frame(matrix(ncol=NCOL(df), nrow=0))
+  for (i in 0:23) {
+    hourlyDfSubset <- df[df$hour==i,]
+    indexOfNextRow <- nrow(resultDf) + 1
+    #resultDf[indexOfNextRow, ] <- t(colMeans(hourlyDfSubset, na.rm = FALSE, dims = 1))
+    resultDf[indexOfNextRow, ] <- columnMax(hourlyDfSubset)
+    rownames(resultDf)[indexOfNextRow] <- i+2
+  }
+  colnames(resultDf) <- colnames(df)
+  return(resultDf)
+}
+
+stationMinsHourly <- function(df) {
+  resultDf <- as.data.frame(matrix(ncol=NCOL(df), nrow=0))
+  for (i in 0:23) {
+    hourlyDfSubset <- df[df$hour==i,]
+    indexOfNextRow <- nrow(resultDf) + 1
+    #resultDf[indexOfNextRow, ] <- t(colMeans(hourlyDfSubset, na.rm = FALSE, dims = 1))
+    resultDf[indexOfNextRow, ] <- columnMin(hourlyDfSubset)
+    rownames(resultDf)[indexOfNextRow] <- i+2
+  }
+  colnames(resultDf) <- colnames(df)
+  return(resultDf)
+}
+
+stationDailyBasicStatistics <- function(df) {
+  resultDf <- as.data.frame(matrix(ncol=NCOL(df), nrow=0))
+  resultDf[1,] <- colMeans(df, na.rm = FALSE, dims = 1)
+  rownames(resultDf)[1] <- "Mean"
+  resultDf[2,] <- columnMax(df)
+  rownames(resultDf)[2] <- "Max"
+  return(resultDf)
+}
+
+linePlotAvailability <- function(mins, maxs, means, stationId) {
+  stationName <- stations[stations$stationId==stationId,]$name
+  y_len <- max(maxs[,stationId]+1)
+  plot(NULL, main=stationName,
+       xlim=c(2,25), ylim=c(0,y_len),
+       #xlim=c(0,23), ylim=c(0,40),
+       xlab="Hour", ylab="Bikes available",
+       lab=c(24, y_len/5, 5), yaxs="i", bg="red")
+  lines(rownames(mins), mins[,stationId], col="#dc0450")
+  lines(rownames(means), means[,stationId], col="#0079c8", lwd=3)
+  lines(rownames(maxs), maxs[,stationId], col="#64be1e")
+}
+
+linePlotAvailabilityAnimate <- function(mins, maxs, means, stations) {
+  for (station in stations) {
+    linePlotAvailability(mins, maxs, means, station)
+    Sys.sleep(2)
+  }
+}
--- main.r
+++ main.r
@@ -3,19 +3,25 @@ rm(list = ls())
 source("getWebData.r")
 source("analysisFunctions.r")
 
-bikeDF <- getWebData("20170625")
+BikeDf2706 <- getWebData("20170627")
+BikeDf2806 <- getWebData("20170628")
 
-# some test variables
-espaAsemat <- c("012", "047", "014", "011", "018", "009", "024", "022", "019")
+allStations <- colnames(BikeDf2706)[1:148]
 
-allStations <- colnames(bikeDF)[1:148]
-
-meansByStation <- stationMeans(bikeDF)
-
-humanReadableObservationTimes <- paste(substring(rownames(bikeDF), 10, 11), ":", substring(rownames(bikeDF), 12, 13), sep="")
-
-#draw_plot_animate(espaAsemat)
+humanReadableObservationTimes <- paste(substring(rownames(BikeDf2706), 10, 11), ":", substring(rownames(BikeDf2706), 12, 13), sep="")
 
 stations <- getBasicStationInfo()
 
-draw_plot_animate(bikeDF, allStations)
+# Tuesday 27.6.
+hourlyAvgs <- stationMeansHourly(BikeDf2706)
+hourlyMaxs <- stationMaxsHourly(BikeDf2706)
+hourlyMins <- stationMinsHourly(BikeDf2706)
+
+linePlotAvailabilityAnimate(hourlyMins, hourlyMaxs, hourlyAvgs, c("078"))
+
+# Wednesday 28.6.
+hourlyAvgs2 <- stationMeansHourly(BikeDf2806)
+hourlyMaxs2 <- stationMaxsHourly(BikeDf2806)
+hourlyMins2 <- stationMinsHourly(BikeDf2806)
+
+linePlotAvailabilityAnimate(hourlyMins2, hourlyMaxs2, hourlyAvgs2, c("078"))
